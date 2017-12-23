c51a01130a8b0c35f4549c2e4918c1e5477f3436
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-12-23 14:48:58 -0500

    truffle config ( set up test network and run truffle migrate )

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Greetings = artifacts.require("./Greetings.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Greetings);
 };
